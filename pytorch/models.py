18d1d95af1b18f43851bc1780237357339bdc04c
Soham Tiwari <soham.tiwari800@gmail.com>
2021-09-08 17:10:17 +0530

    added print statements in forward function of Cnn_9layers_Gru_FrameAtt

--- pytorch/models.py
+++ pytorch/models.py
@@ -575,7 +575,8 @@ class Cnn_9layers_Gru_FrameAtt(nn.Module):
             n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin, top_db=top_db,
             freeze_parameters=True)
 
-        self.fbank_s3prl = torch.hub.load('s3prl/s3prl', 'fbank')
+        self.s3prl_feature_type = 'spectrogram'
+        self.extractor_s3prl = torch.hub.load('s3prl/s3prl', self.s3prl_feature_type)
 
         # STFT extractor
 #        self.stft_extractor = STFT(n_fft=window_size, hop_length=hop_size, win_length=window_size,
@@ -625,13 +626,17 @@ class Cnn_9layers_Gru_FrameAtt(nn.Module):
         wavs = [wav for wav in x.squeeze(1)]
         print(f"\n\n wavs {len(wavs)}")
         print(wavs)
-        print(f"#### DEBUGGING ENDS #########")
-        print(f"#############################")
+
+        out_s3prl = self.extractor_s3prl(wavs)['last_hidden_state']
+        print(f"\n\n {self.s3prl_feature_type}:\n{out_s3prl}")
+
 
 
         if self.feature_type == 'logmel':
             x = self.spectrogram_extractor(x)   # (batch_size, 1, time_steps, freq_bins)
+            print(f"\n\nspectrogram librosa:\n{x}")
             x = self.logmel_extractor(x)    # (batch_size, 1, time_steps, mel_bins)
+            print(f"\n\nlogmel spectrogram librosa:\n{x}")
         elif self.feature_type == 'cqt':
             x = self.stft_extractor(x)
             x = self.cqt_extractor(x)
@@ -641,20 +646,32 @@ class Cnn_9layers_Gru_FrameAtt(nn.Module):
             x = x.transpose(2,3)
             x = x.to('cuda')
 
+        print(f"Before transpose 1:\n{x}")
         x = x.transpose(1, 3)
+        print(f"Before bn0 1:\n{x}")
         x = self.bn0(x)
         x = x.transpose(1, 3)
+        print(f"After transpose 2:\n{x}")
 
         if self.training:
+            print(f"Before spec_augmenter:\n{x}")
             x = self.spec_augmenter(x)
+            print(f"After spec_augmenter:\n{x}")
+
 
         # Mixup on spectrogram
         if self.training and mixup_lambda is not None and not timeshift:
+            print(f"Before do_mixup :\n{x}")
             x = do_mixup(x, mixup_lambda)
+            print(f"After do_mixup :\n{x}")
+
 
         if self.training and mixup_lambda is not None and timeshift:
+            print(f"Before do_mixup_timeshift :\n{x}")
             x = do_mixup_timeshift(x, mixup_lambda)
+            print(f"After do_mixup_timeshift :\n{x}")
 
+        print(f"Before conv input :\n{x}")
         x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
         x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
@@ -681,6 +698,8 @@ class Cnn_9layers_Gru_FrameAtt(nn.Module):
             'clipwise_output': clipwise_output,
             'embedding': cla}
 
+        print(f"#### DEBUGGING ENDS #########")
+        print(f"#############################")
         return output_dict
 
 
