69afb786d7ece0986d8ad18aa6d6fc7e5be163a2
Soham Tiwari <soham.tiwari800@gmail.com>
2022-04-20 02:22:30 +0530

    fix: corrected model summary input size

--- pytorch/main_strong.py
+++ pytorch/main_strong.py
@@ -195,7 +195,7 @@ def train(args):
 
     statistics_container = StatisticsContainer(statistics_path)
     if model_summary:
-        summary(model, (time_steps, mel_bins), "cpu")
+        summary(model, (16000,), "cpu")
 
     if resume_iteration:
         resume_checkpoint_path = os.path.join(checkpoints_dir, checkpoint_name)
